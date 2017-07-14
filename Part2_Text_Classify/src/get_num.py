8d09f80af4ebf2f1a9a11008f818648c51f70481
sunhang <1072302787@qq.com>
2017-07-14 17:52:18 +0800

    add

--- Part2_Text_Classify/src/get_num.py
+++ Part2_Text_Classify/src/get_num.py
@@ -5,16 +5,24 @@ import os
 def get_has_num():
     file_path = '../data_cls_re'
     in_file_list = os.listdir(file_path)
+    word_in_file_stat = {}
     for file_name in in_file_list:
-        in_file = open(file_path +"/"+ file_name, 'r', encoding='utf-8')
+        in_file = open(file_path + "/" + file_name, 'r', encoding='utf-8')
         data = in_file.readline().split(",")[1]
 
-
-
         data_temp = data.split(" ")
-        word_in_file_stat = {}
         for word in data_temp:
-            print(word)
+            # print(word)
+            if word not in word_in_file_stat and word != '':
+                word_in_file_stat[word] = []
+                word_in_file_stat[word].append(data_temp.count(word))
+
+    out_file = open('re.txt', 'w', encoding='utf-8')
+    out_file.write(str(word_in_file_stat))
+    # for (k, v) in word_in_file_stat.items():
+    #     print(k+v)
+    #     out_file.write(v + ',' + k + ' ' + '\n')
+    # out_file.close()
 
 
 if __name__ == '__main__':
